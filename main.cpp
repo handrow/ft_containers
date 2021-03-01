7d2d732d17f1fae232775f26e68e0b1b1aa6c457
U29maXEgIFNoaWRsb3dza2FxIA== U2hpZGxvd3NrYXEg <handrow@am-f3.msk.21-school.ru>
2021-03-01 22:48:45 +0300

    added erase, insert, swap

--- main.cpp
+++ main.cpp
@@ -1,108 +1,520 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   main.cpp                                           :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: handrow <handrow@student.42.fr>            +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/02/17 17:06:18 by handrow           #+#    #+#             */
-/*   Updated: 2021/02/28 20:50:23 by handrow          ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
-#include <vector>
-#include <iostream>
-#include <memory>
+#include "randomAccessIterator.hpp"
 #include "vector.hpp"
+#include <vector>
+#include <list>
+
+void test_assing()
+{
+	std::cout << "TEST ASSIGN" << std::endl;
+
+	ft::vector<int> test1;
+
+	test1.push_back(10);
+	test1.assign(4, 1);
+	test1.push_back(10);
+	test1.push_back(11);
+	ft::vector<int>::iterator it1 = test1.begin();
+	ft::vector<int>::iterator it2 = test1.end();
+	ft::vector<int>::iterator it3 = test1.begin();
+	while(it1 != it2)
+	{
+		std::cout << *it1 << ",";
+		it1++;
+	}
+	std::cout << std::endl;
+	std::cout << std::endl;
+	ft::vector<int> test2;
+
+	test2.assign(it3, it2);
+	test2.push_back(10);
+	test2.pop_back();
+	it3 = test2.begin();
+	ft::vector<int>::iterator it4 = test2.end();
+	while(it3 != it4)
+	{
+		std::cout << *it3 << ",";
+		it3++;
+	}
+	std::cout << std::endl;
+	std::cout << std::endl;
+	std::cout << "*******************************" << std::endl;
+	std::cout << std::endl;
+
+}
+
+void test_insert()
+{
+	std::cout << "TEST INSERT" << std::endl;
+	ft::vector<int> test1;
+
+	for (int i = 9; i < 20; i++)
+		test1.push_back(i);
+	ft::vector<int>::iterator it1 = test1.begin();
+
+	// ft::vector<int>::iterator it5 = test1.insert(it1 + 1, 5);
+	test1.insert(it1 + 2, 10);
+	it1 = test1.begin();
+	ft::vector<int>::iterator it2 = test1.end();
+	
+	it1 = test1.begin();
+	it2 = test1.end();
+
+	while(it1 != it2)
+	{
+		std::cout << *it1 << ",";
+		it1++;
+	}
+	std::cout << "\n";
+
+	ft::vector<int> test2;
+
+	for (int i = 20; i < 30; i++)
+		test2.push_back(i);
+	it1 = test1.begin();
+	ft::vector<int>::iterator it7 = test2.begin();
+	test2.insert(it7 + 3, it1, it2);
+	it7 = test2.begin();
+	it7 = test2.begin();
+	it7 = test2.begin();
+	ft::vector<int>::iterator it8 = test2.end();
+	while(it7 != it8)
+	{
+		std::cout << *it7 << ",";
+		it7++;
+	}
+
+	std::cout << std::endl;
+	std::cout << std::endl;
+	std::cout << "*******************************" << std::endl;
+	std::cout << std::endl;
+
+
+	std::vector<int> t;
+	for (int i = 10; i < 20; i++)
+		t.push_back(i);
+	std::vector<int>::iterator it3 = t.begin();
+	// std::vector<int>::iterator it6 = t.end();
+	it3++;
+	std::vector<int>::iterator it4 = t.insert(it3, 5);
+	for (unsigned long i = 0; i < t.size(); i++)
+		std::cout << t[i] << " ";
+	std::cout << *it4;
+}
+
+// void test_arase()
+// {
+// 	std::cout << "TEST ERASE" << std::endl;
+
+// 	std::vector<int> test1;
+
+// 	for (int i = 10; i < 20; i++)
+// 		test1.push_back(i);
+// 	for(unsigned long i = 0; i < test1.size(); i++)
+// 		std::cout << test1[i] << " ";
+// 	std::cout << std::endl;
+// 	std::vector<int>::iterator it1 = test1.begin();
+// 	it1 = test1.erase(it1 + 1);
+// 	for(unsigned long i = 0; i < test1.size(); i++)
+// 		std::cout << test1[i] << " ";
+
+// 	std::cout << "\nreturn iterator " << *it1 << std::endl;
+// 	std::cout << std::endl;
+
+// 	ft::vector<int> test2;
+
+// 	for (int i = 10; i < 20; i++)
+// 		test2.push_back(i);
+// 	for(unsigned long i = 0; i < test2.size(); i++)
+// 		std::cout << test2[i] << " ";
+// 	std::cout << std::endl;
+// 	ft::vector<int>::iterator it2 = test2.begin();
+// 	it2 = test2.erase(it2 + 1);
+// 	for(unsigned long i = 0; i < test2.size(); i++)
+// 		std::cout << test2[i] << " ";
+
+// 	std::cout << "\nreturn iterator " << *it2 << std::endl;
+// 	std::cout << std::endl;
+// 	std::cout << std::endl;
+
+// 	std::vector<int> test3;
+
+// 	for (int i = 10; i < 20; i++)
+// 		test3.push_back(i);
+// 	for(unsigned long i = 0; i < test3.size(); i++)
+// 		std::cout << test3[i] << " ";
+// 	std::cout << std::endl;
+// 	std::vector<int>::iterator it3 = test3.begin();
+// 	it3 = test3.erase(it3 + 1, it3 + 3);
+// 	for(unsigned long i = 0; i < test3.size(); i++)
+// 		std::cout << test3[i] << " ";
+
+// 	std::cout << "\nreturn iterator " << *it3 << std::endl;
+// 	std::cout << std::endl;
+
+// 	ft::vector<int> test4;
+
+// 	for (int i = 10; i < 20; i++)
+// 		test4.push_back(i);
+// 	for(unsigned long i = 0; i < test4.size(); i++)
+// 		std::cout << test4[i] << " ";
+// 	std::cout << std::endl;
+// 	ft::vector<int>::iterator it4 = test4.begin();
+// 	it4 = test4.erase(it4 + 1, it4 + 3);
+// 	for(unsigned long i = 0; i < test4.size(); i++)
+// 		std::cout << test4[i] << " ";
+	
+// 	std::cout << "\nreturn iterator " << *it4 << std::endl;
+
+// 	std::cout << std::endl;
+// 	std::cout << std::endl;
+// 	std::cout << "*******************************" << std::endl;
+// 	std::cout << std::endl;
+// }
+
+// void	test_swap()
+// {
+// 	std::cout << "TEST SWAP" << std::endl;
+
+// 	std::vector<int>	test1(5, 200);
+// 	std::vector<int>	test2(5, 100);
+
+// 	for (unsigned long i = 0; i < test1.size(); i++)
+// 		std::cout << test1[i] << " ";
+// 	std::cout << std::endl;
+// 	for (unsigned long i = 0; i < test2.size(); i++)
+// 		std::cout << test2[i] << " ";
+// 	test1.swap(test2);
+// 	std::cout << std::endl;
+// 	std::cout << std::endl;
+// 	for (unsigned long i = 0; i < test1.size(); i++)
+// 		std::cout << test1[i] << " ";
+// 	std::cout << std::endl;
+// 	for (unsigned long i = 0; i < test2.size(); i++)
+// 		std::cout << test2[i] << " ";
+	
+// 	std::cout << std::endl;
+// 	std::cout << std::endl;
+
+// 	ft::vector<int>	test3(5, 200);
+// 	ft::vector<int>	test4(5, 100);
+
+// 	for (unsigned long i = 0; i < test3.size(); i++)
+// 		std::cout << test3[i] << " ";
+// 	std::cout << std::endl;
+// 	for (unsigned long i = 0; i < test4.size(); i++)
+// 		std::cout << test4[i] << " ";
+// 	std::cout << std::endl;
+// 	test3.swap(test4);
+// 	std::cout << std::endl;
+// 	for (unsigned long i = 0; i < test3.size(); i++)
+// 		std::cout << test3[i] << " ";
+// 	std::cout << std::endl;
+// 	for (unsigned long i = 0; i < test4.size(); i++)
+// 		std::cout << test4[i] << " ";
+
+// 	std::cout << std::endl;
+// 	std::cout << std::endl;
+// 	std::cout << "*******************************" << std::endl;
+// 	std::cout << std::endl;
+// }
+
+// void test_clear()
+// {
+// 	ft::vector<int> myvector;
+// 	myvector.push_back (100);
+// 	myvector.push_back (200);
+// 	myvector.push_back (300);
+
+// 	std::cout << "myvector contains:";
+//   for (unsigned i=0; i<myvector.size(); i++)
+//     std::cout << ' ' << myvector[i];
+//   std::cout << '\n';
+
+//   myvector.clear();
+//   myvector.push_back (1101);
+//   myvector.push_back (2202);
+
+//   std::cout << "myvector contains:";
+//   for (unsigned i=0; i<myvector.size(); i++)
+//     std::cout << ' ' << myvector[i];
+//   std::cout << '\n';
+// }
+
+// void test_at()
+// {
+// 	ft::vector<int> myvector (10, 21);   // 10 zero-initialized ints
+
+//   // assign some values:
+// 	for (unsigned i = 0; i < myvector.size(); i++)
+// 	myvector.at(i) = i;
+
+// 	std::cout << "myvector contains:";
+// 	for (unsigned i = 0; i < myvector.size(); i++)
+// 		std::cout << ' ' << myvector.at(i);
+// 	std::cout << '\n';
+// }
+
+// void test_reserve()
+// {
+// 	std::cout << "TEST RESERVE" << std::endl;
+
+// 	ft::vector<int> test;
+
+// 	for (int i = 0; i < 7; i++)
+// 		test.push_back(i);
+// 	std::cout << test.capacity();
+// 	std::cout << std::endl;
+// 	test.reserve(8);
+// 	std::cout << test.capacity();
+// 	std::cout << std::endl;
+// 	test.reserve(16);
+// 	std::cout << test.capacity();
+// 	std::cout << std::endl;
+// 	for (unsigned long i = 0; i < test.size(); i++)
+// 		std::cout << test[i] << " ";
+
+// 	std::cout << std::endl;
+// 	std::cout << std::endl;
+// 	std::cout << "*******************************" << std::endl;
+// 	std::cout << std::endl;
+// }
+
+// void test_reverse_iterator()
+// {
+// 	std::cout << "TEST REVERSE ITERATOR" << std::endl;
+// 	ft::vector<int> test1;
+
+// 	for (int i = 0; i < 11; i++)
+// 		test1.push_back(i);
+	
+// 	ft::vector<int>::reverse_iterator it1 = test1.rbegin();
+// 	std::cout << *it1 << " ";
+
+// 	std::vector<int> test2;
+
+// 	for (int i = 0; i < 11; i++)
+// 		test2.push_back(i);
+
+// 	std::vector<int>::reverse_iterator it2 = test2.rbegin();
+// 	std::cout << *it2 << " ";
+
+// 	ft::vector<int>::reverse_iterator it3(test1.rbegin());
+
+// 	std::cout << std::endl;
+// 	std::cout << std::endl;
+// 	std::cout << "*******************************" << std::endl;
+// 	std::cout << std::endl;
+// }
+
+// void test_base()
+// {
+// 	std::cout << "TEST BASE" << std::endl;
+// 	std::cout << std::endl;
+
+// 	ft::vector<int> v;
+
+// 	for (unsigned long i = 0; i < 6; i++)
+// 		v.push_back(i);
+
+//     ft::vector<int>::iterator it = v.begin() + 3;
+//     ft::vector<int>::reverse_iterator r_it = v.rend() - 3;
+ 
+//     std::cout << "*it == " << *it << ", *r_it.base() == " << *r_it.base()
+//     << '\n' << "*r_it == " << *r_it <<", *(r_it.base()-1) == " << *(r_it.base()-1) << "\n";
+// 	std::cout << std::endl;
+
+// 	std::vector<int> v1;
+
+// 	for (unsigned long i = 0; i < 6; i++)
+// 		v1.push_back(i);
+
+//     std::vector<int>::iterator it1 = v1.begin() + 3;
+//     std::vector<int>::reverse_iterator r_it1 = v1.rend() - 3;
+ 
+//     std::cout << "*it == " << *it1 << ", *r_it.base() == " << *r_it1.base()
+//     << '\n' << "*r_it == " << *r_it1 <<", *(r_it.base()-1) == " << *(r_it1.base()-1) << "\n";
+  
+// //   {
+// //     RevIt r_end(v.begin());
+// //     RevIt r_begin(v.end());
+ 
+// //     for (auto it = r_end.base(); it != r_begin.base(); ++it) {
+// //       std::cout << *it << " ";
+// //     }
+// //     std::cout << "\n";
+// //   }
+// 	std::cout << std::endl;
+
+// 	ft::vector<int> myvector;
+// 	for (int i=0; i<10; i++) myvector.push_back(i);
+
+// 	typedef ft::vector<int>::iterator iter_type;
+
+// 	ft::reverse_iterator<iter_type> rev_end (myvector.begin());
+// 	ft::reverse_iterator<iter_type> rev_begin (myvector.end());
+
+// 	std::cout << "myvector:";
+// 	for (iter_type it = rev_end.base(); it != rev_begin.base(); ++it)
+// 		std::cout << ' ' << *it;
+// 	std::cout << '\n';
+
+// 	std::vector<int> myvector1;
+// 	for (int i=0; i<10; i++) myvector1.push_back(i);
+
+// 	typedef std::vector<int>::iterator iter_type1;
+
+// 	std::reverse_iterator<iter_type1> rev_end1 (myvector1.begin());
+// 	std::reverse_iterator<iter_type1> rev_begin1 (myvector1.end());
+
+// 	std::cout << "myvector:";
+// 	for (iter_type1 it = rev_end1.base(); it != rev_begin1.base(); ++it)
+// 		std::cout << ' ' << *it;
+// 	std::cout << '\n';
+
+// 	std::cout << std::endl;
+// 	std::cout << std::endl;
+// 	std::cout << "*******************************" << std::endl;
+// 	std::cout << std::endl;
+// }
+
+// void test_resize()
+// {
+// 	std::vector<int> test1(5, 5);
+// 	for (unsigned long i = 0; i < test1.size(); i++)
+// 		std::cout << test1[i] << " ";
+// 	std::cout << std::endl;
+// 	test1.resize(10, 7);
+// 	for (unsigned long i = 0; i < test1.size(); i++)
+// 		std::cout << test1[i] << " ";
+// 	std::cout << std::endl;
+// 	test1.resize(4);
+// 	for (unsigned long i = 0; i < test1.size(); i++)
+// 		std::cout << test1[i] << " ";
+// 	std::cout << std::endl;
+// 	test1.resize(5);
+// 	for (unsigned long i = 0; i < test1.size(); i++)
+// 		std::cout << test1[i] << " ";
+// 	std::cout << std::endl;
+
+// 	ft::vector<int> test2(5, 5);
+// 	for (unsigned long i = 0; i < test2.size(); i++)
+// 		std::cout << test2[i] << " ";
+// 	std::cout << std::endl;
+// 	test2.resize(10, 7);
+// 	for (unsigned long i = 0; i < test2.size(); i++)
+// 		std::cout << test2[i] << " ";
+// 	std::cout << std::endl;
+// 	test2.resize(4);
+// 	for (unsigned long i = 0; i < test2.size(); i++)
+// 		std::cout << test2[i] << " ";
+// 	std::cout << std::endl;
+// 	test2.resize(5);
+// 	for (unsigned long i = 0; i < test2.size(); i++)
+// 		std::cout << test2[i] << " ";
+// 	std::cout << std::endl;
+// }
+
+void test_constructor()
+{
+	std::cout << "TEST CONSTRUCTOR" << std::endl;
+	std::cout << std::endl;
+	ft::vector<int> test1(5, 6);
+
+	for (unsigned long i = 0; i < test1.size(); i++)
+		std::cout << test1[i] << " ";
+	std::cout << std::endl;
+	
+	ft::vector<int> test2(test1);
+	for (unsigned long i = 0; i < test2.size(); i++)
+		std::cout << test2[i] << " ";
+	std::cout << std::endl;
+	
+	test2[2] = 2;
+	ft::vector<int>::iterator it = test2.begin();
+	ft::vector<int> test3(it + 1, it + 4);
+
+	for (unsigned long i = 0; i < test3.size(); i++)
+		std::cout << test3[i] << " ";
+	std::cout << std::endl;
+	std::cout << std::endl;
+
+	std::vector<int> test4(5, 6);
+	test4[2] = 2;
+	std::vector<int>::iterator it1 = test4.begin();
+	std::vector<int> test5(it1 + 1, it1 + 4);
+
+	for (unsigned long i = 0; i < test5.size(); i++)
+		std::cout << test5[i] << " ";
+
+	std::cout << std::endl;
+	std::cout << std::endl;
+	std::cout << "*******************************" << std::endl;
+	std::cout << std::endl;
+
+}
+
 int main()
 {
-    std::vector<std::string> vec;
-    vec.push_back("S");
-    vec.push_back("S");
-    vec.push_back("S");
-    vec.at(2);
-    for (int i = 0; i < vec.size(); ++i)
-        std::cout << vec[i] << std::endl;
-    vec.assign(3, "Sam");
-    for (int i = 0; i < vec.size(); ++i)
-        std::cout << vec[i] << std::endl;
-    std::allocator<int> alloc;
-    alloc.allocate(8);
-    int *a = alloc.allocate(1);
-    alloc.construct(a, 5);
-    std::cout << "__________________________________________" << std::endl;
+	//test_constructor();
+	//test_assing();
+	//test_insert();
+	// test_arase();
+	// test_swap();
+	// test_clear();
+	// test_at();
+	// test_reserve();
+	// test_reverse_iterator();
+	// test_base();
+	// test_resize();
+
+	// std::cout << "MY INSERT ITERATOR TEST" << std::endl;
+
+	// ft::vector<int> test;
+	// for (int i = 1; i < 10; i++)
+	// 	test.push_back(i);
+	// for (int i = 0; i < test.size(); i++)
+	// 	std::cout << test[i] << " ";
+	// ft::vector<int>::iterator it = test.begin();
+	// std::cout << std::endl;
+
+	// ft::vector<int> test2;
+	// for (int i = 9; i > 0; i--)
+	// 	test2.push_back(i);
+	// for (int i = 0; i < test2.size(); i++)
+	// 	std::cout << test2[i] << " ";
+	// ft::vector<int>::iterator it1 = test2.begin();
+	// std::cout << std::endl;
 
 
-    std::vector<int> bb;
-    bb.reserve(5);
-    bb.push_back(2);
-    bb.push_back(2);
-    bb.push_back(2);
-    bb.push_back(2);
-    bb.push_back(2);
-    bb.reserve(10);
-    bb.reserve(20);
-        
-    std::cout << bb.size() << "|" << bb.capacity() << std::endl;
-    std::cout << std::endl;
-    
-    std::cout << "MY VECTOR" << std::endl;
-    std::cout << std::endl;
+	// test2.insert(it1 + 1, it + 1, test.end());
+	// for (int i = 0; i < test2.size(); i++)
+	// 	std::cout << test2[i] << " ";
 
-    // ft::vector<int> c;
-    // std::cout << c.size() << "|" << c.capacity() << std::endl;
-    // c.reserve(2);
-    // std::cout << c.size() << "|" << c.capacity() << std::endl;
-    // c.push_back(2);
-    // c.push_back(2);
-    // c.push_back(2);
-    // c.push_back(2);
-    // c.push_back(2);
-    // c.pop_back();
-    // c.resize(20);
-    // std::cout << c.size() << "|" << c.capacity() << std::endl;
-    // for (int i = 0; i < c.size(); ++i)
-    //     std::cout << c[i] << " ";
-    // std::cout << std::endl;
-    // std::cout << "push elem" << std::endl;
-    // ft::vector<int>::iterator   it = c.begin();
-    // ft::vector<int>::iterator   id = c.end();
-    // //while (it != id)
-    // //    std::cout << *it++ << " ";
-    // std::cout << std::endl;
-    
-    
-    std::cout << "BASE TEST" << std::endl;
-    ft::vector<int> vc;
-    vc.push_back(1);
-    vc.push_back(2);
-    vc.push_back(3);
-    vc.push_back(4);
-    vc.push_back(5);
-    vc.push_back(6);
-    std::cout << "BEGIN" << std::endl;
-     for (int i = 0; i < vc.size(); ++i)
-        std::cout << vc[i] << " ";
-   // std::vector<int>::iterator it = vc.begin();
-    ft::vector<int>::iterator id = vc.begin();
-//     vc.insert(id + 1, 100);
-//     std::cout << "FINAL" <<std::endl;
-//    for (int i = 0; i < vc.size(); ++i)
-//         std::cout << vc[i] << " ";
-    vc.insert(id + 2, 5, 100);
-        std::cout << "FINAL" <<std::endl;
-   for (int i = 0; i < vc.size(); ++i)
-        std::cout << vc[i] << " ";
-    std::cout << std::endl;
+	// std::cout << "TEST OPERATOR=" << std::endl;
 
+	// ft::vector<int>	test;
+	// for (int i = 1; i < 10; i++)
+	// 	test.push_back(i);
+	// ft::vector<int> test2 = test;
+	// // test2.push_back(1);
+	// // test2 = test;
+	// for(int i = 0; i < test2.size(); i++)
+	// 	std::cout << test2[i] << " ";
+	// while(1){}
 
-    std::cout << "TEST INSERT OUT OF RANGE" << std::endl;   
-    std::vector<int> hh;
-    hh.push_back(1);
-    hh.push_back(2);
-    std::vector<int>::iterator iterq = hh.begin();
-    hh.insert(iterq + 5, 100);
-      for (int i = 0; i < hh.size(); ++i)
-        std::cout << hh[i] << " ";
-    
+	std::cout << "TEST ERASE POS" << std::endl;
+	ft::vector<int> d;
+	for (int i = 1; i < 10; i++)
+		d.push_back(i);
+	for (int i = 0; i < d.size(); i++)	
+		std::cout << d[i] << " ";
+	std::cout<< "|" << d.size() << "|" << " BEGIN SIZE" << std::endl;
+	ft::vector<int>::iterator	ie = d.begin();
+
+	//d.reserve(20); //check it
+	d.erase(ie + 4, ie + 9);
+	for (int i = 0; i < d.size(); i++)	
+		std::cout << d[i] << " ";
+	std::cout<< "|" << d.size() << "|" << " END SIZE" << std::endl;
+	return 0;
 }
--- vector.hpp
+++ vector.hpp
@@ -6,7 +6,7 @@
 /*   By: handrow <handrow@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/17 17:06:14 by handrow           #+#    #+#             */
-/*   Updated: 2021/02/28 20:47:05 by handrow          ###   ########.fr       */
+/*   Updated: 2021/03/01 22:47:44 by handrow          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 #include <memory>
 #include <exception>
-#include <limits>
+#include <iostream>
 #include "randomAccessIterator.hpp"
 
 namespace ft
@@ -43,7 +43,6 @@ namespace ft
         size_type                                   length;
         size_type                                   cap;
 
-        
         // HELP FUNCTIONS
         size_type   recommend(size_type new_size)
         {
@@ -54,6 +53,11 @@ namespace ft
                 throw std::length_error("length error");
             if (_cap >= ms / 2)
                 return std::max<size_type>(2 * _cap, new_size);
+        }
+
+        void       construct_at_end(size_type idx, const_reference value)
+        {
+            allocator.allocate(&data[idx], value_type(value));
         }
 
     public:
@@ -100,17 +104,20 @@ namespace ft
         size_type           size() const                    { return length; }
         size_type           max_size() const                { return allocator.max_size(); }
         size_type           capacity() const                { return cap; }
-        void                reserve(size_type new_cap);
+        void                reserve(size_type new_cap); // check if reserve works correct
 
         // MODIFIERS
 
         void                clear();
         iterator            insert(iterator pos, const_reference value);
         void                insert(iterator pos, size_type count, const_reference value);
-       // void                insert(iterator pos, iterator first, iterator last);
+        void                insert(iterator pos, iterator first, iterator last);
+        iterator            erase(iterator pos);
+        iterator            erase(iterator first, iterator last);
         void                push_back(const_reference value);
         void                pop_back();
         void                resize(size_type count, value_type value = value_type());
+        void                swap(vector& other);
 
         // NON-MEMBER FUNCTIONS
 
@@ -120,17 +127,19 @@ namespace ft
         friend bool         operator<=(const vector& lhs, const vector& rhs);
         friend bool         operator>(const vector& lhs, const vector& rhs);
         friend bool         operator>=(const vector& lhs, const vector& rhs);
+
     };
 
     template<typename T, typename Alloca>
     vector<T, Alloca>::vector(const allocator_type& alloc)
     : allocator(alloc), data(NULL), length(0), cap(0)
     {
+        // std::cout << "DEFAULT CALLED" << std::endl;
     }
 
     template<typename T, typename Alloca>
     vector<T, Alloca>::vector(size_type n, const_reference val, const allocator_type& alloc)
-    : allocator(alloc), data(alloc.allocate(n)), length(n), cap(n)
+    : allocator(alloc), data(allocator.allocate(n)), length(n), cap(n)
     {
         for (size_type i = 0; i < n; ++i)
             allocator.construct(&data[i], val);
@@ -138,15 +147,18 @@ namespace ft
 
     template<typename T, typename Alloca>
     vector<T, Alloca>::vector(const vector& src)
-    : allocator(src.alloc), data(src.data), length(src.length), cap(src.cap)
+    // : allocator(src.allocator), data(src.data), length(src.length), cap(src.cap)
+    // {
+    //     for (size_type i = 0; i < length; ++i)
+    //         allocator.construct(&data[i], src.data[i]);
+    // }
     {
-        for (size_type i = 0; i < length; ++i)
-            allocator.construct(&data[i], src.data[i]);
+        *this = src;
     }
     
     template<typename T, typename Alloca>
     vector<T, Alloca>::vector(iterator first, iterator last, const allocator_type& alloc)
-    : allocator(alloc), data(alloc.allocate(last - first)), length(last - first), cap(last - first)
+    : allocator(alloc), data(allocator.allocate(last - first)), length(last - first), cap(last - first)
     {
         assign(first, last);
     }
@@ -163,15 +175,19 @@ namespace ft
         }
     }
 
+
+// empty test = test2 
+// default constructor doesnt called 
     template<typename T, typename Alloca>
     vector<T, Alloca>&  vector<T, Alloca>::operator=(const vector& src)
     {
-        if (*this == src)
-            return *this;
-        this->~vector(); // delete this->data
-        allocator = src.alloca;
+        // if (*this == src) // does not work
+        //     return *this;
+         if (data)//????? operator doesnt work!!!!!! GG
+             this->~vector(); // delete this->data
+        allocator = src.allocator;
         data = allocator.allocate(src.cap); // create new data
-        for (size_type i = 0; i < src.length; ++i)
+         for (size_type i = 0; i < src.length; ++i)
             allocator.construct(&data[i], src.data[i]);
         length = src.length;
         cap = src.cap;
@@ -260,7 +276,7 @@ namespace ft
         }
         else
         {
-            const size_type tmpCap = recommend(tmpCap);//cap > (ms / 2) ? ms : cap * 2;
+            const size_type tmpCap = cap > (ms / 2) ? ms : cap * 2;
             pointer         tmpData = allocator.allocate(tmpCap);
             memcpy(tmpData, data, insertIdx * sizeof(value_type));
             memcpy(tmpData + insertIdx + 1, data + insertIdx, (length - insertIdx) * sizeof(value_type));
@@ -272,10 +288,6 @@ namespace ft
         }
         return pos;
     }
-
-// 1 2 3 4 5
-// 1 2 3 4 2 3 4 5
-// 1 x x x 2 3 4 5
 
     template<typename T, typename Alloca>
     void    vector<T, Alloca>::insert(iterator pos, size_type count, const_reference value)
@@ -310,6 +322,73 @@ namespace ft
             cap = tmpCap;
         }
     }
+
+//first != last, then pos != end
+
+    template<typename T, typename Alloca>
+    void                vector<T, Alloca>::insert(iterator pos, iterator first, iterator last)
+    {
+        size_type size = last - first;
+        size_type insertIdx = pos - begin();
+        const size_type ms = max_size();
+        
+        if (data == NULL && length == 0)
+        {
+            data = allocator.allocate(1);
+            cap = 1;
+        }
+
+        else if (cap >= length + size)
+        {
+            memmove(data + insertIdx + size, data + insertIdx, (length - insertIdx) * sizeof(value_type));
+            while (first != last)
+                allocator.construct(&data[insertIdx++], *first++);
+            while (pos != end())
+                allocator.construct(&data[insertIdx++], *pos++);
+            length += size;
+        }
+        else
+        {
+            const size_type tmpCap = std::max(cap > (ms / 2) ? ms : cap * 2, length + size);
+            pointer         tmpData = allocator.allocate(tmpCap);
+            memcpy(tmpData, data, insertIdx * sizeof(value_type));
+            memcpy(tmpData + insertIdx + size, data + insertIdx, (length - insertIdx) * sizeof(value_type));
+            while (first != last)
+                allocator.construct(&tmpData[insertIdx++], *first++);
+            while (pos != end())
+                allocator.construct(&tmpData[insertIdx++], *pos++);
+            length += size;
+            allocator.deallocate(data, cap);
+            data = tmpData;
+            cap = tmpCap;
+        }
+    }
+
+    template<typename T, typename Alloca>
+    typename vector<T, Alloca>::iterator    vector<T, Alloca>::erase(iterator pos)
+    {
+        size_type   eraseIdx = pos - begin();
+        memmove(data + eraseIdx + 1, data + eraseIdx, (length - eraseIdx) * sizeof(value_type));
+        length += 1;
+        memmove(data + eraseIdx, data + eraseIdx + 2, (length - eraseIdx - 1) * sizeof(value_type));
+        length -= 2;
+        return pos;
+    }
+
+    template<typename T, typename Alloca>
+    typename vector<T, Alloca>::iterator    vector<T, Alloca>::erase(iterator first, iterator last)
+    {
+        size_type   size = last - first;
+
+        memmove(data + *first - 1, data + *last - size + 1, (length - size) * sizeof(value_type));
+        length -= size;
+        return last;
+    }
+// 1 2 3 4 5 6 7 8 9 |9| BEGIN SIZE
+// 3 4 5 6 7 8 9    |7|  END SIZE
+
+// 1 2 3 4 5 6 7 8 9 |9| BEGIN SIZE
+// 1 2 5 6 7 8 9     |7| END SIZE
 
     template<typename T, typename Alloca>
     void    vector<T, Alloca>::push_back(const_reference value)
@@ -348,4 +427,19 @@ namespace ft
             allocator.construct(&data[length++], value);
     }
 
+    template<typename T, typename Alloca>
+    void    vector<T, Alloca>::swap(vector& other)
+    {
+        vector<T, Alloca> tmp = *this;
+        *this = other;
+        other = tmp;
+    }
+
+    template<typename T, typename Alloca>
+    void    swap(vector<T, Alloca>& lhs, vector<T, Alloca>& rhs)
+    {
+        vector<T, Alloca> tmp = lhs;
+        lhs = rhs;
+        rhs = tmp;
+    }
 } // namespace ft
