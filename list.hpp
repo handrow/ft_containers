9f41f8e749ecdc5e7daaee7692f92ae92c0ebfe6
U29maXEgIFNoaWRsb3dza2FxIA== U2hpZGxvd3NrYXEg <handrow@am-e3.msk.21-school.ru>
2021-03-10 19:51:53 +0300

    added unique remove sort

--- list.hpp
+++ list.hpp
@@ -6,7 +6,7 @@
 /*   By: handrow <handrow@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/03 16:04:38 by handrow           #+#    #+#             */
-/*   Updated: 2021/03/09 21:09:57 by handrow          ###   ########.fr       */
+/*   Updated: 2021/03/11 15:48:05 by handrow          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,12 @@
 
 namespace ft
 {
+    template <typename T>
+    bool pred(const T &a , const T &b)
+    {
+        return a == b;
+    }
+
     template <typename T>
     bool cmp(const T &a, const T &b)
     {
@@ -100,25 +106,74 @@ namespace ft
         void                swap(list& other);
 
         // OPERATIONS
+        void                splice(const_iterator pos, list& other);
+        //void                splice(const_iterator pos, list& other, const_iterator it);
+        //void                splice(const_iterator pos, list& other, const_iterator first, const_iterator last);
+        void                remove(const_reference value);
+        // check remove_if
+        template<class UnaryPredicate>
+        void                remove_if(UnaryPredicate pred)
+        {
+            iterator    first = begin();
+            iterator    last = end();
+            
+            while (first != last)
+            {
+                if (pred(*first))
+                    first = erase(first);
+                else
+                    first++;
+            }
+        }
+        void                reverse();
+        void                unique();
+        template<class BinaryPredicate>
+        void                unique(BinaryPredicate p)
+        {
+            if (_size > 1)
+            {
+                iterator    i1 = begin();
+                iterator    i2 = i1;
+
+                while (++i1 != end())
+                {
+                    if (p(*i1,*i2))
+                    {
+                        erase(i1);
+                        i1 = i2;
+                    }
+                    else
+                        i2 = i1;
+                }
+            }
+        }
         void                sort();
+        template <typename Compare>
+        void 				sort(Compare comp)
+        {
+            if (_size > 1)
+            {
+                iterator i1 = begin();
+                iterator i2 = i1;
+                iterator i3 = i2;
 
-        // template <typename Compare>
-        // void 				sort(Compare comp)
-        // {
-        //     iterator it = begin();
-        //     iterator it2 = begin();
-        //     iterator it_end = end();
-
-        //     while (++it != it_end)
-        //     {
-        //         if (comp(*it, *it2))
-        //         {
-        //             std::swap(*it, it2);   
-        //         }
-        //         else
-        //             ++it2;
-        //     }
-        // }
+                while(i1 != end())
+                {
+                    i2 = i1;
+                    i1++;
+                    i3 = i1;
+                    while(i1 != end() && !comp(*i2,*i1))
+                    {
+                        std::swap(*i1, *i2);
+                        if (i2 == begin())
+                            break ;
+                        i1 = i2;
+                        --i2;
+                    }
+                    i1 = i3;
+                }
+            }
+        }
     };
 
     template<typename T, typename Alloca>
@@ -195,6 +250,11 @@ namespace ft
     template<typename T, typename Alloca>
     typename list<T, Alloca>::iterator      list<T, Alloca>::erase(iterator pos)
     {
+        if (pos == begin())
+        {
+            pop_front();
+            return begin();
+        }
         node    *ptr = pos->prev->next;
         pos->prev->next = pos->next;
         pos->next->prev = pos->prev;
@@ -270,30 +330,71 @@ namespace ft
     }
 
     template<typename T, typename Alloca>
+    void        list<T, Alloca>::splice(const_iterator pos, list& other)
+    {
+        if (!other.empty())
+        {
+            node *tmp = pos->prev;
+            pos->prev->next = other._head;
+            pos->prev = other._tail;
+            other._head->prev = tmp;
+            other._tail->next = tmp->next;
+            _size += other._size;
+            other._head = other._tail;
+            other._size = 0;
+        }
+    }
+
+    template<typename T, typename Alloca>
+    void        list<T, Alloca>::remove(const_reference value)
+    {
+        iterator first = begin();
+        iterator last = end();
+
+        while (first != last)
+        {
+            if (*first == value)
+               first = erase(first);
+            else
+                first++;
+        }
+    }
+
+    template<typename T, typename Alloca>
+    void        list<T, Alloca>::reverse()
+    {
+        iterator first = begin();
+        iterator last = end();
+
+        for (size_type i = 0; i < _size; i += 2)
+            std::swap(*first++, *--last);
+    }
+
+    template<typename T, typename Alloca>
+    void        list<T, Alloca>::unique()
+    {
+    if (_size > 1)
+    {
+        iterator    i1 = begin();
+        iterator    i2 = i1;
+
+        while (++i1 != end())
+        {
+            if (*i1 == *i2)
+            {
+                erase(i1);
+                i1 = i2;
+            }
+            else
+                i2 = i1;
+        }
+    }
+}
+
+    template<typename T, typename Alloca>
     void       list<T, Alloca>::sort()
     {
-        if (_size > 1)
-        {
-            iterator i1 = begin();
-            iterator i2 = i1;
-            iterator i3 = i2;
-
-            while(i1 != end())
-            {
-                i2 = i1;
-                i1++;
-                i3 = i1;
-                while(i1 != end() && *i1 < *i2)
-                {
-                    std::swap(*i1, *i2);
-                    if (i2 == begin())
-                        break ;
-                    i1 = i2;
-                    --i2;
-                }
-                i1 = i3;
-            }
-        } 
+        sort(cmp<T>); // WHY DOES IT WORK
     }
 
 } // namespace ft
--- main_list.cpp
+++ main_list.cpp
@@ -2,31 +2,69 @@
 #include "list.hpp"
 #include <iostream>
 
-int main()
+
+void test_splice()
 {
     ft::list<int> l;
-    for (int i = 11; i > 1; i--)
-        l.push_back(i);
-    for (int i = 0; i < 20; i++)
-        l.push_back(i);
-    //l.pop_back();
-    //l.pop_front();
-    //l.push_front(30);
-    //l.push_front(40);
-    //l.assign(2, 10);
+    ft::list<int> l1;
+
+    l.push_back(100);
+    l.push_back(100);
+    l.push_back(100);
+
     ft::list<int>::iterator it1 = l.begin();
     ft::list<int>::iterator it2 = l.end();
-   // l.erase(++it1);
-    //it1--;
-    //l.insert(it1, 50); // does not work if i insert value to begin()
-    //it1 = l.begin();
     while (it1 != it2)
         std::cout << *it1++ << " ";
-    l.sort();
-    std::cout << std::endl;
-    ft::list<int>::iterator it12 = l.begin();
-    ft::list<int>::iterator it21 = l.end();
-        while (it12 != it21)
-        std::cout << *it12++ << " ";
-    std::cout << std::endl << l.size();
+
+    l1.push_back(3);
+    l1.push_back(3);
+    l1.push_back(2);
+
+    ft::list<int>::iterator it3 = l1.begin();
+    ft::list<int>::iterator it4 = l1.end();
+    while (it3 != it4)
+        std::cout << *it1++ << " ";
+
+    l.splice(it2--, l1);
+    ft::list<int>::iterator it5 = l.begin();
+    ft::list<int>::iterator it6 = l.end();
+    while (it5 != it6)
+        std::cout << *it5++ << " ";
+}
+
+int main()
+{
+//     ft::list<int> l;
+//     //for (int i = 11; i > 1; i--)
+//      //   l.push_back(i);
+//     //for (int i = 0; i < 20; i++)
+//      //   l.push_back(i);
+//      l.push_back(100);
+//      l.push_back(100);
+//      l.push_back(100);
+//      l.push_back(1);
+//      l.push_back(1);
+//      l.push_back(2);
+//      l.push_back(50);
+//      l.push_back(50);
+
+//     ft::list<int>::iterator it1 = l.begin();
+//     ft::list<int>::iterator it2 = l.end();
+//    // l.erase(++it1);
+//     //it1--;
+//     //l.insert(it1, 50); // does not work if i insert value to begin()
+//     //it1 = l.begin();
+//     while (it1 != it2)
+//         std::cout << *it1++ << " ";
+//     //l.remove(100);
+//    //l.remove_if([](int n){ return n > 10; });  check remove if
+//     std::cout << std::endl;
+//     ft::list<int>::iterator it12 = l.begin();
+//     ft::list<int>::iterator it21 = l.end();
+//         while (it12 != it21)
+//         std::cout << *it12++ << " ";
+//     std::cout << std::endl << l.size();
+
+    test_splice();
 }
